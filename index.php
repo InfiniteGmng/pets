5bccd6e85c35e38c379690e0142f6edd7615000c
Noah Lanctot <noah.lanctot@gmail.com>
2024-01-18 13:56:54 -0500

    Added index.php file

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+
+require_once('vendor/autoload.php');
+
+
+$f3 = Base::instance();
+
+
+
+$f3->route('GET /', function () {
+    echo "<h1>Pet Home</p>";
+});
+
+
+$f3->run();
